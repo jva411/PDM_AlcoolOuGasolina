b5abcf32bad3840cd0bed0932f08c81699da8a7a
jva411 <j.victoralves411@gmail.com>
2023-09-27 10:42:31 -0300

    corrigindo cálculos, adicionando verificação do valor, salvando e recuperando o valor digitado

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -2,21 +2,29 @@ package com.example.alcoolougasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.CompoundButton
 import android.widget.EditText
 import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity: AppCompatActivity() {
     var percentual: Double = 0.7
+    var valorGasolina: Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("PDM23","No onCreate, $percentual")
+        valorGasolina = savedInstanceState?.getDouble("valor")
+        if (valorGasolina != null && valorGasolina != -0.0) {
+            Log.d("PDM23","Value recuperated: $valorGasolina")
+            findViewById<EditText>(R.id.edGasolina).setText(valorGasolina.toString())
+            findViewById<TextView>(R.id.tvAlcool).text = "Preço Competitivo R\$%.2f".format(percentual * valorGasolina!!)
+        }
+        findViewById<Switch>(R.id.swPercentual).isChecked = savedInstanceState?.getBoolean("checked") ?: false
 
         val btCalc = findViewById<Button>(R.id.btCalcular)
         btCalc.setOnClickListener(View.OnClickListener {
@@ -24,8 +32,13 @@ class MainActivity: AppCompatActivity() {
 
             val edGasolina = findViewById<EditText>(R.id.edGasolina)
             val tvAlcool = findViewById<TextView>(R.id.tvAlcool)
-            val valorAlcool = percentual * edGasolina.text.toString().toDouble()
-            tvAlcool.text = "Preço Competitivo R\$%.2f".format(valorAlcool)
+            try {
+                valorGasolina = edGasolina.text.toString().toDouble()
+                val valorGasolina = percentual * valorGasolina!!
+                tvAlcool.text = "Preço Competitivo R\$%.2f".format(valorGasolina)
+            } catch (ex: Exception) {
+                tvAlcool.text = "Valor inválido!"
+            }
         })
 
         val swPercentual = findViewById<Switch>(R.id.swPercentual)
@@ -35,6 +48,13 @@ class MainActivity: AppCompatActivity() {
             percentual = if (checked) 0.75 else 0.7
             swPercentual.text = "%d%%".format((percentual * 100.0).toInt())
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d("PDM23","Save value: ${valorGasolina ?: -0.0}")
+        outState.putDouble("valor", valorGasolina ?: -0.0)
+        outState.putBoolean("checked", findViewById<Switch>(R.id.swPercentual).isChecked)
+        super.onSaveInstanceState(outState)
     }
 
     override fun onResume() {
